5823109f633a619ad59347c5cd5c96201e6e39ec
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-07-15 08:08:38 +0300

    Hide term 1 degrees when screen less than 970

--- js/script.js
+++ js/script.js
@@ -25,27 +25,28 @@ fetch('https://iabdwahab.me/darelom-first-degrees-2024/data.json').then(res => r
   let idInterval = setInterval(() => {
     if (!typeof data[i].degrees !== 'string') {
       tableEl.innerHTML += `
-      <tr id="student-${i}">
-        <td class="order-el">${i + 1}</td>
-        <td class="id-el">${data[i].id}</td>
-        <td class="name-el">${data[i].name}</td>
-        <td>${data[i].degrees[0] || 0}</td>
-        <td>${data[i].degrees[1] || 0}</td>
-        <td>${data[i].degrees[2] || 0}</td>
-        <td>${data[i].degrees[3] || 0}</td>
-        <td>${data[i].degrees[4] || 0}</td>
-        <td>${data[i].degrees[5] || 0}</td>
-        <td>${data[i].degrees[6] || 0}</td>
-        <td>${data[i].degrees[7] || 0}</td>
-        <td>${data[i].degrees[8] || 0}</td>
-        <td>${data[i].degrees[9] || 0}</td>
-        <td>${data[i].degrees[10] || 0}</td>
-        <td>${data[i].degrees[11] || 0}</td>
-        <td>${data[i].degrees[12] || 0}</td>
-        <td>${data[i].degrees[13] || 0}</td>
-        <td class="total-el">${data[i].total}</td>
-        <td class="percentage-el">${(data[i].total * 100 / 1200).toFixed(2)}%</td>
-      </tr>
+        <tr id="student-${i}">
+          <td class="order-el">${i + 1}</td>
+          <td class="id-el">${data[i].id}</td>
+          <td class="name-el">${data[i].name}</td>
+          <td class="term-2">${data[i].degrees[0] || 0}</td>
+          <td class="term-2">${data[i].degrees[1] || 0}</td>
+          <td class="term-2">${data[i].degrees[2] || 0}</td>
+          <td class="term-2">${data[i].degrees[3] || 0}</td>
+          <td class="term-1">${data[i].degrees[4] || 0}</td>
+          <td class="term-2">${data[i].degrees[5] || 0}</td>
+          <td class="term-1">${data[i].degrees[6] || 0}</td>
+          <td class="term-2">${data[i].degrees[7] || 0}</td>
+          <td class="term-1">${data[i].degrees[8] || 0}</td>
+          <td class="term-2">${data[i].degrees[9] || 0}</td>
+          <td class="term-1">${data[i].degrees[10] || 0}</td>
+          <td class="term-1">${data[i].degrees[11] || 0}</td>
+          <td class="term-2">${data[i].degrees[12] || 0}</td>
+          <td class="term-2">${data[i].degrees[13] || 0}</td>
+
+          <td class="total-el">${data[i].total}</td>
+          <td class="percentage-el">${(data[i].total * 100 / 1200).toFixed(2)}%</td>
+        </tr>
       `;
     }
 
